452e46ad7f92794694c52a196c24b88d35db65d2
Daniel_ecc <dancord12072002@gmail.com>
2025-12-05 18:03:19 -0500

    Adicion de DTO para ver historial de compra con consultas sql

---
+++ Api de Prueba/Modelos/DTO/OrderDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Api_de_Prueba.Modelos.DTO
+{
+    public class OrderDto
+    {
+        [JsonPropertyName("orderId")]
+        public int numeroCompra { get; set; }
+
+        [JsonPropertyName("userId")]
+        public int usuarioId { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime fechaCompra { get; set; }
+
+        [JsonPropertyName("total")]
+        public decimal totalAmount { get; set; }
+
+        [JsonPropertyName("itemsCount")]
+        public int itemsCount { get; set; }
+    }
+}
--- Api de Prueba/obj/Debug/net8.0/Api de Prueba.AssemblyInfo.cs
+++ Api de Prueba/obj/Debug/net8.0/Api de Prueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api de Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1029ce6ed60d1fb7f77d3aff71be76b7d5b3f803")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0b3a27bcd524414cccbf608400553fae1c120d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api de Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api de Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
